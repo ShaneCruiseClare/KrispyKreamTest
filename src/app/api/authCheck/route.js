6ac093d52986e3eb8bc302e5448938e4b17af5e8
ShaneCruiseClare <114066730+ShaneCruiseClare@users.noreply.github.com>
2023-12-14 20:39:11 +0000

    test

--- src/app/api/authCheck/route.js
+++ src/app/api/authCheck/route.js
@@ -5,7 +5,9 @@ export async function GET(req, res) {
     // Make a note we are on
     // the api. This goes to the console.
     console.log("checking auth")
-    
+
+    const res2 = await fetch('http://localhost:3000/api/checkAuth’’')
+    const data = await res2.json()
     // get the auth record
     let record = cookies().get('auth');
     console.log(record.value);
